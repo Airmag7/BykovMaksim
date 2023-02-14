da8678b5b52e6e426ee7ad712acb6dc88b685843
Mac <mac@MacBook-Pro-Mac.local>
2023-02-14 00:12:01 -0800

    1 и 2

---
+++ tasks/task2.py
@@ -0,0 +1,23 @@
+#prin a
+
+#1 = 1
+
+#print (a)
+
+#print ("sdvbsbdlvblsbv
+#aoivoibabvb")
+
+#print ('bdu' - 3)
+
+#while h!=0:
+#h = h-1
+
+#if true:
+#        print ('1')
+#    print ('1')
+
+#import math
+#h = math.sqrt(-9)
+
+#import math
+#1-math.exp(-4*1000000*-0.0641515994108)
